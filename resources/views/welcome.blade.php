62607479c342bd4226e9fae9cd1f5643edfb82c7
thaimirc-debug <thaimirc@gmail.com>
2025-10-31 17:19:58 +0700

    update
    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
                     @else
                         <a
                             href="{{ route('login') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
+                            class="btn btn-success"
                         >
                             Log in
                         </a>
